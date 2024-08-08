7b3ee885498555e5ba3c02bd3910b566752d0214
huyvo-rec <huy.vo@recruitery.co>
2024-08-08 11:15:26 +0700

    update1

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local'],
     }),
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   connectionName: "test",
-    //   useFactory: async (configService: ConfigService) => ({
-    //     uri: "mongodb+srv://19521641:19521641@the-movie-film.yiwcjtd.mongodb.net/?retryWrites=true&w=majority",
-    //   }),
-    //   inject: [ConfigService],
-    // }),
-    MongooseModule.forRoot("mongodb://adminIT:CC26696736153F43F32C9B8FFF524@ec2-18-139-53-51.ap-southeast-1.compute.amazonaws.com:27597/rrdb_dev?authSource=admin", { useFindAndModify: false }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: "test",
+      useFactory: async (configService: ConfigService) => ({
+        uri: "mongodb+srv://19521641:19521641@the-movie-film.yiwcjtd.mongodb.net/?retryWrites=true&w=majority",
+      }),
+      inject: [ConfigService],
+    }),
+    // MongooseModule.forRoot("mongodb://adminIT:CC26696736153F43F32C9B8FFF524@ec2-18-139-53-51.ap-southeast-1.compute.amazonaws.com:27597/rrdb_dev?authSource=admin", { useFindAndModify: false }),
     // ArticleModule,
   ],
   controllers: [AppController],
