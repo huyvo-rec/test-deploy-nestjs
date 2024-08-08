de5dc484dd5d172479b9b0dbaf54426db793c36b
huyvo-rec <huy.vo@recruitery.co>
2024-08-08 23:51:39 +0700

    sdfg

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -3,12 +3,12 @@ import { ArticleController } from './article.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleSchema } from './schemas/article.schema';
 import { ArticleService } from './article.service';
-import { ArticleModuleV2 } from '../articlev2/article.module';
+// import { ArticleModuleV2 } from '../articlev2/article.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Article', schema: ArticleSchema }]),
-    ArticleModuleV2
+    // ArticleModuleV2
   ],
   controllers: [ArticleController],
   providers: [ArticleService],
