fff8fa8e923f39239550267d4534bd1b66d1f9b2
huyvo-rec <huy.vo@recruitery.co>
2024-08-08 23:40:07 +0700

    update

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -3,7 +3,7 @@ import { ArticleController } from './article.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleSchema } from './schemas/article.schema';
 import { ArticleService } from './article.service';
-import { ArticleModuleV2 } from 'src/articlev2/article.module';
+import { ArticleModuleV2 } from '../articlev2/article.module';
 
 @Module({
   imports: [
